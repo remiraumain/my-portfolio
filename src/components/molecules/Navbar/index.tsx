5ec38146ba5cee67bbdc04acead7b849e05ebe9a
Rémi Raumain <remiraumain@gmail.com>
2023-04-07 16:28:28 +0200

    3D setup

--- src/components/molecules/Navbar/index.tsx
+++ src/components/molecules/Navbar/index.tsx
@@ -2,7 +2,7 @@ import NavLinks from "~/components/atoms/NavLinks";
 
 const Navbar = () => {
   return (
-    <nav className="fixed inset-x-0 bottom-10 mx-auto max-w-max rounded-md bg-black p-1.5">
+    <nav className="fixed inset-x-0 bottom-10 z-10 mx-auto max-w-max rounded-md bg-black p-1.5">
       <NavLinks pages={["hello-world", "projects", "contact", "game"]} />
     </nav>
   );
--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -1,5 +1,7 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import { Canvas } from "@react-three/fiber";
+import Experience from "~/components/3d/Experience";
 
 const Game: NextPage = () => {
   return (
@@ -9,7 +11,20 @@ const Game: NextPage = () => {
         <meta name="description" content="Check out my project" />
       </Head>
       <main>
-        <div>Game</div>
+        <div className={"fixed left-0 top-0 h-screen w-screen bg-sky-600"}>
+          <Canvas
+            shadows
+            camera={{
+              fov: 45,
+              near: 0.1,
+              far: 200,
+              position: [2.5, 4, 6],
+            }}
+          >
+            <ambientLight color={"white"} intensity={0.3} />
+            <Experience />
+          </Canvas>
+        </div>
       </main>
     </>
   );
